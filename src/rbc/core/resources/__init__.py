71617a3f8a48dee91687ec3d79db832838a2c120
Jason Kai <21226986+kaitj@users.noreply.github.com>
2026-02-02 10:14:29 -0500

    Add note about image origin

--- src/rbc/core/resources/__init__.py
+++ src/rbc/core/resources/__init__.py
@@ -6,6 +6,7 @@ from types import SimpleNamespace
 RESOURCES_DIR = Path(__file__).parent.resolve()
 
 # OASIS
+# (sourced from C-PAC container ghcr.io/fcp-indi/c-pac:many_pipes)
 OASIS_DIR = RESOURCES_DIR / "oasis"
 OASIS_TEMPLATES = SimpleNamespace(
     template=OASIS_DIR / "T_template0.nii.gz",
@@ -13,6 +14,7 @@ OASIS_TEMPLATES = SimpleNamespace(
     registration_mask=OASIS_DIR / "T_template0_BrainCerebellumRegistrationMask.nii.gz",
 )
 # MNI152
+# (sourced from FSL6.0 in C-PAC container: ghcr.io/fcp-indi/c-pac:many_pipes
 MNI_DIR = RESOURCES_DIR / "mni"
 MNI_TEMPLATES = SimpleNamespace(brain_1mm=MNI_DIR / "MNI152_T1_1mm_brain.nii.gz")
 
