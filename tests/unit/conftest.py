1ab90162617a79e940bbb06cdd0dc608fa1d1ce4
Jason Kai <21226986+kaitj@users.noreply.github.com>
2026-02-06 12:44:04 -0500

    Add fixture to conftest, add tests for utils.rename

---
+++ tests/unit/conftest.py
@@ -0,0 +1,13 @@
+"""Fixtures specific to unit tests."""
+
+import pathlib as pl
+
+import pytest
+
+
+@pytest.fixture
+def test_file(tmp_path: pl.Path) -> pl.Path:
+    """Create sample file for testing."""
+    test_file = tmp_path / "test_file.txt"
+    test_file.write_text("Sample content")
+    return test_file
