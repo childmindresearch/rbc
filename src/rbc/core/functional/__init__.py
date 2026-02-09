e51a1eaf115b93b8d359ef31c1260475cb439279
Janhavi Pillai <janhavi.pillai@gmail.com>
2026-02-09 11:50:55 -0500

    format func init

--- src/rbc/core/functional/__init__.py
+++ src/rbc/core/functional/__init__.py
@@ -10,4 +10,4 @@ must be corrected before analysis.
 from .initialization import scale, truncate_trs
 from .motion import generate_motion_reference, motion_correction
 
-__all__ = ["truncate_trs", "scale", "generate_motion_reference", "motion_correction"]
+__all__ = ["generate_motion_reference", "motion_correction", "scale", "truncate_trs"]
