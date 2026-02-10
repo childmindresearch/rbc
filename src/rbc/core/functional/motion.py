bae12e1f273b43fa6e2103434f5ff4a08583bf32
Janhavi Pillai <janhavi.pillai@gmail.com>
2026-02-09 21:00:05 -0500

    use namedtuple in motion_correction & match pyproject.toml

--- src/rbc/core/functional/motion.py
+++ src/rbc/core/functional/motion.py
@@ -1,8 +1,9 @@
 """RBC motion reference & correction."""
 
 from pathlib import Path
-from types import SimpleNamespace
+from typing import NamedTuple
 
+import nibabel as nib
 from niwrap import afni, fsl
 
 
@@ -16,9 +17,9 @@ def generate_motion_reference(in_file: Path, output_fname: str) -> afni.V3dcalcO
     Returns:
         AFNI 3dcalc output object.
     """
-    total_vols = afni.v_3dinfo(dataset=[in_file], nv=True)
+    total_vols = nib.load(in_file).shape[3]
 
-    mid_vol = (int(total_vols.info[0])) // 2
+    mid_vol = total_vols // 2
 
     return afni.v_3dcalc(
         dataset_a=afni.v_3dcalc_dataset_a_file(file=in_file, selectors_=f"[{mid_vol}]"),
@@ -27,9 +28,19 @@ def generate_motion_reference(in_file: Path, output_fname: str) -> afni.V3dcalcO
     )
 
 
+class MotionCorrectedOutputs(NamedTuple):
+    """NamedTuple for motion correction outputs."""
+
+    bold: Path
+    par: Path
+    rms_rel: Path
+    rms_abs: Path
+    mat_dir: Path
+
+
 def motion_correction(
     in_file: Path, ref_file: Path, output_prefix: str
-) -> SimpleNamespace:
+) -> MotionCorrectedOutputs:
     """Estimate and correct head motion using FSL mcflirt.
 
     Args:
@@ -38,7 +49,7 @@ def motion_correction(
         output_prefix: Prefix for output files.
 
     Returns:
-        Namespace with paths to BOLD motion corrected outputs and matrices.
+        NamedTuple with paths to motion corrected outputs and matrices.
     """
     mc_result = fsl.mcflirt(
         in_file=in_file,
@@ -50,16 +61,12 @@ def motion_correction(
         out_file=output_prefix,
     )
 
-    motion_mat_dir = None
-    for d in Path(mc_result.root).iterdir():
-        if d.is_dir() and d.suffix == ".mat":
-            motion_mat_dir = d
-            break
+    motion_mat_dir = Path(mc_result.root) / f"{output_prefix}.mat"
 
-    if motion_mat_dir is None:
-        raise FileNotFoundError(f"Missing .mat directory in {mc_result.root}")
+    if not motion_mat_dir.exists():
+        raise FileNotFoundError(f"Missing .mat directory at {motion_mat_dir}")
 
-    return SimpleNamespace(
+    return MotionCorrectedOutputs(
         bold=Path(mc_result.out_file),
         par=Path(mc_result.par_file),
         rms_rel=Path(mc_result.rmsrel_files),
