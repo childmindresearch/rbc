8f86a3654380bd7273d73358ad5500d9e91b6680
Florian Rupprecht <33600480+nx10@users.noreply.github.com>
2026-02-10 16:49:25 -0500

    Apply suggestion from @nx10

--- src/rbc/core/nifti.py
+++ src/rbc/core/nifti.py
@@ -18,8 +18,6 @@ __all__ = ["nifti_num_volumes"]
 
 def nifti_num_volumes(in_file: str | Path) -> int:
     """Return the number of volumes in a NIfTI image (returns 1 for 3-D images).
-
-    Used to find the middle volume index for motion-reference extraction.
     """
     shape = nibabel.nifti1.load(in_file).shape
     return shape[3] if len(shape) > 3 else 1
