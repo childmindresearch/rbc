22151fe3e88f9fce4648adc0e6bd04d5227e3638
Jason Kai <21226986+kaitj@users.noreply.github.com>
2026-02-19 08:50:55 -0500

    Add _DEFAULT_ENV_VARS

--- src/rbc/cli/anatomical.py
+++ src/rbc/cli/anatomical.py
@@ -1,3 +1,68 @@
 """CLI subcommand for anatomical processing."""
 
 from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import argparse
+    from collections.abc import Sequence
+
+from pathlib import Path
+
+import polars as pl
+from tqdm import tqdm
+
+from rbc.cli import _DEFAULT_ENV_VARS
+from rbc.core.bids2table import load_table
+from rbc.core.niwrap import setup_runner
+from rbc.workflows.anatomical import single_session_preprocess
+
+
+def main(args: argparse.Namespace) -> int:
+    """Main entrypoint of anatomical workflow."""
+    group_entity = ("sub", "ses", "run")
+
+    # Setup
+    ctx = setup_runner(runner=args.runner, verbose=args.verbose)
+    ctx.runner.environ = _DEFAULT_ENV_VARS
+
+    ctx.logger.info("Preparing to run RBC anatomical workflow")
+    df = load_table(
+        dataset_dir=args.input_dir, index_fpath=None, max_workers=0, verbose=ctx.verbose
+    )
+
+    filters = [
+        pl.col("datatype") == "anat",
+        pl.col("suffix") == "T1w",
+        pl.col("ext").str.contains(".nii"),
+    ]
+    if args.participant_label is not None:
+        filters.append(pl.col("sub").is_in(args.participant_label))
+    if args.session_label is not None:
+        filters.append(pl.col("ses").is_in(args.session_label))
+    df = df.filter(pl.all_horizontal(filters))
+
+    for _, group in tqdm(df.group_by(group_entity), disable=not ctx.verbose):
+        for row in group.iter_rows(named=True):
+            t1w_fpath = Path(row["root"]) / row["path"]
+            ctx.logger.info(f"Processing {t1w_fpath}")
+            single_session_preprocess(in_t1w=t1w_fpath, output_dir=args.output_dir)
+
+    ctx.logger.info("RBC anatomical workflow complete")
+    return 0
+
+
+def register_command(
+    subparsers: argparse._SubParsersAction, parents: Sequence[argparse.ArgumentParser]
+) -> None:
+    """Register anatomical workflow to parser."""
+    parser = subparsers.add_parser(
+        "anatomical",
+        parents=parents,
+        description="RBC anatomical workflow",
+        help="Anatomical workflow",
+        usage="rbc input_dir output_dir anatomical [-h] [options]",
+    )
+
+    parser.set_defaults(func=lambda args: main(args))
