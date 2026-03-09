7e2187d8c4a983eef0d8d658e3137cc9cfb20ec8
Jason Kai <21226986+kaitj@users.noreply.github.com>
2026-03-09 10:37:38 -0400

    Fix ruff issue

--- src/rbc/context.py
+++ src/rbc/context.py
@@ -130,7 +130,7 @@ class PipelineContext:
         return dest
 
     def ensure_dataset_description(self) -> None:
-        """Create dataset_description.json file in the output directory if it doesn't exist."""
+        """Create dataset_description.json in output directory if it doesn't exist."""
         _ensure_dataset_description(output_dir=self.output_dir)
 
 
